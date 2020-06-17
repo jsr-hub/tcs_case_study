3b87f6126ca58bdeaf90a6770f50bf58283bc0a8
jsr-hub <jsanjayblossom@gmail.com>
2020-06-18 03:12:10 +0530

    update

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 from flask_mysqldb import MySQL
 import yaml
 
@@ -15,6 +15,10 @@ app.config['MYSQL_DB'] = db['mysql_db']
 
 mysql = MySQL(app)
 z=0
+account=None
+aid=None
+cid=None
+transamt=None
 account=None
 duser=None
 user=None
@@ -187,10 +191,13 @@ def createacctpage():
             #print(AccountType)
             DepAmount=userDetails['amount']
             cur = mysql.connection.cursor()
-          
-            print("s")
-            cur.execute("insert into Account (ws_cust_id,ws_acct_type,ws_acct_balance) values(%s,%s,%s)",(Customer_id,AccountType,DepAmount))
-            mysql.connection.commit()
+            
+            z=cur.execute("select * from Account where ws_cust_id=%s and ws_acct_type=%s",(Customer_id,AccountType))
+            if z==0:
+                cur.execute("insert into Account (ws_cust_id,ws_acct_type,ws_acct_balance) values(%s,%s,%s)",(Customer_id,AccountType,DepAmount))
+                mysql.connection.commit()
+            else:
+                return ("Account Exist")
             dataval=cur.execute("select * from Account where ws_cust_id=%s",[Customer_id])
             if dataval>0:
                 print("MOER THAN WON")
@@ -198,7 +205,7 @@ def createacctpage():
                 print(singledet[1])
                 
                 Message="Account Created Successfully"
-                cur.execute("insert into AccountStatus (ws_cust_id,ws_acct_id,ws_acct_type,Status,Message) values(%s,%s,%s,%s,%s)",(Customer_id,singledet[1],AccountType,singledet[3],Message))
+                cur.execute("insert into AccountStatus (ws_cust_id,ws_acct_id,ws_acct_type,Status,Message) values(%s,%s,%s,%s,%s)",(Customer_id,singledet[1],AccountType,"Active",Message))
                 mysql.connection.commit()
             cur.close()
             return "SUCCESS:"
@@ -239,6 +246,10 @@ def deleteacctpage():
     global account
     return render_template('07 Delete Account.html',actid=account)
 
+
+
+
+
 @app.route('/accountsearch/<int:check>',methods=["GET","POST"])
 def searchacc(check):
     if request.method == 'POST':
@@ -247,21 +258,34 @@ def searchacc(check):
         custid=None
         acctid=userDetails['actid']
         custid=str(userDetails['cstid'])
-        print(acctid)
-        print(custid)
+        global cid
+        cid=custid
+        global aid
+        aid=acctid
         cur = mysql.connection.cursor()
         resultValue=cur.execute("select * from Account where ws_acct_id=%s OR ws_cust_id like %s",[acctid,custid])
         global account
         if resultValue>0:
             account=cur.fetchone()
+            print(account)
+            cid=account[0]
+            aid=account[1]
             #return render_template('08 Account Status.html',userdet=dets)    
+            ctr = mysql.connection.cursor()
+            global customer
+            resValue=ctr.execute("select * from Customer where ws_cust_id=%s",[account[0]])
+            if resValue>0:
+                customer=ctr.fetchone()
+                print(customer)
             if check==1:
                 #return render_template('07 Delete Account.html',userdet=dets)
-                return redirect('/deleteacctpage')
+                return redirect(url_for('deleteacctpage'))
             elif check==2:
-                return render_template('11 Deposit Amount.html',userdet=dets)
+                return redirect(url_for('confirmdep'))
             elif check==3:
-                return render_template('12 Withdraw Amount.html',userdet=dets)
+                return redirect(url_for('confirmwit'))
+            elif check==4:
+                return redirect(url_for('confirmtrn'))
         else:
             return "Customer Doesnt Exist"
         #return render_template('08 Account Status.html',userdet=dets)    
@@ -269,6 +293,158 @@ def searchacc(check):
 
 
 
+
+
+@app.route('/depositmoney')
+def deposit():
+    chk=2
+    return redirect(url_for('searchacc',check = chk))
+
+
+@app.route('/depositafter',methods=["GET","POST"])
+def confirmdep():
+    global cid
+    global aid
+    global account
+    if request.method == 'POST':
+        userDetails = request.form
+        acctid=int(userDetails['acct'])
+        updamt=userDetails['depamt']
+        cur = mysql.connection.cursor()
+        cur.execute("update Account set ws_acct_balance = ws_acct_balance + %s where ws_acct_id=%s",[updamt,acctid])
+        mysql.connection.commit()
+        stringval="Deposited "+updamt+" from Account"
+        global transamt
+        transamt=updamt
+        cur.execute("update AccountStatus set Message = %s where ws_acct_id=%s",[stringval,acctid])
+        mysql.connection.commit()
+        srctype='c'
+        transtype='d'
+        cur.execute("insert into transactions (ws_acct_id,ws_trxn_type,ws_amt,ws_src_act_type,ws_dest_act_type) values(%s,%s,%s,%s,%s)",(aid,transtype,transamt,srctype,account[2]))
+        mysql.connection.commit()
+        cid=None
+        aid=None
+        transamt=None
+        account=None
+        return "Deposited Successfully"
+    return render_template('11 Deposit Amount.html',actid=account)
+                
+@app.route('/withdrawmoney')
+def withdraw():
+    chk=3
+    return redirect(url_for('searchacc',check = chk))
+
+@app.route('/withdrawafter',methods=["GET","POST"])
+def confirmwit():
+    global cid
+    global aid
+    global account
+    if request.method == 'POST':
+        userDetails = request.form
+        acctid=int(userDetails['acct'])
+        updamt=userDetails['depamt']
+        cur = mysql.connection.cursor()
+        z=cur.execute("select * from Account where ws_acct_id=%s and ws_acct_balance - %s >-1 ",[acctid,updamt])
+        if z==0:
+            return("insufficient balance")
+        cur.execute("update Account set ws_acct_balance = ws_acct_balance - %s where ws_acct_id=%s",[updamt,acctid])
+        mysql.connection.commit()
+        stringval="Withdrawn "+updamt+" from Account"
+        global transamt
+        transamt=updamt
+        cur.execute("update AccountStatus set Message = %s where ws_acct_id=%s",[stringval,acctid])
+        mysql.connection.commit()
+        srctype='c'
+        transtype='w'
+        cur.execute("insert into transactions (ws_acct_id,ws_trxn_type,ws_amt,ws_src_act_type,ws_dest_act_type  ) values(%s,%s,%s,%s,%s)",(aid,transtype,transamt,srctype,account[2]))
+        mysql.connection.commit()
+        cid=None
+        aid=None
+        transamt=None
+        account=None
+        return "Withdrawn Successfully"
+    return render_template('12 Withdraw Amount.html',actid=account)
+
+@app.route('/transfermoney')
+def transfer():
+    chk=4
+    return redirect(url_for('searchacc',check = chk))
+
+@app.route('/transferafter',methods=["GET","POST"])
+def confirmtrn():
+    global cid
+    global aid
+    global customer
+    global account
+    #print(customer)
+    if request.method == 'POST':
+        userDetails = request.form
+        custid=int(userDetails['acctid'])
+        custname=userDetails['actname']
+        srctype=userDetails['srcact']
+        dsttype=userDetails['dstact']
+        if(srctype=='Current Account'):
+            srctype='c'
+            src='Current Account'
+        elif(srctype=='Savings Account'):
+            srctype='s'
+            src='Savings Account'
+        if(dsttype=='Current Account'):
+            dsttype='c'
+            dst='Current Account'
+        elif(dsttype=='Savings Account'):
+            dsttype='s'
+            dst='Savings Account'
+        print(srctype)
+        tramt=userDetails['amt']
+        cur = mysql.connection.cursor()
+        stringval="Transferred "+tramt+"from "+src+" to "+dst
+        global transamt
+        transamt=tramt
+        transtype='t'
+        cur.execute("insert into transactions (ws_acct_id,ws_trxn_type,ws_amt,ws_src_act_type,ws_dest_act_type) values(%s,%s,%s,%s,%s)",(aid,transtype,tramt,srctype,dsttype))
+        mysql.connection.commit()
+        cid=None
+        aid=None
+        transamt=None
+        customer=None
+        return "Transferred Successfully"
+    return render_template('13 Transfer Money.html',custid=customer)
+
+@app.route('/accountstatementA',methods=["GET","POST"])
+def accountstatementA():
+    global aid
+    if request.method == 'POST':
+        userDetails = dict(request.form)
+        print(userDetails)
+        aid=userDetails["acctid"]
+        if(userDetails["exampleRadios"]=="no"):
+            cur = mysql.connection.cursor()
+            z=cur.execute("select * from account where ws_acct_id =%s",[aid])
+            if z==0:
+                return("NO SUCH ACCOUNT EXIST");
+            n=int(userDetails["no"])
+            z=cur.execute("select * from transactions where ws_acct_id=%s ORDER BY ws_rxn_date desc LIMIT %s",(aid,n))
+            if z>0:
+                res=cur.fetchall()
+                return render_template('14a Acount Statement.html',userDetails=res)
+            else:
+                return("NO TRANSACTIONS YET")
+            
+        else:
+            return redirect('/accountstatementB')
+        cur.close()
+    return render_template('14a Acount Statement.html')
+
+@app.route('/accountstatementB',methods=["GET","POST"])
+def accountstatementB():
+    global aid
+    if request.method == 'POST':
+        userDetails = dict(request.form)
+        print(userDetails)
+        
+            
+    return render_template('14a Acount Statement.html')
 
 
 if __name__ == '__main__':
